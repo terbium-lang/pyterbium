f11a5cd955088e1835a3f9e04347efc3bc8420e4
cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-08-06 14:23:09 -0700

    parser basic

---
+++ src/parser.py
@@ -0,0 +1,18 @@
+from rply import ParserGenerator, Token
+from .state import State
+
+from typing import TypeVar, List
+
+from .lexer import LexerGenerator
+
+T = TypeVar('T', bound=List[Token])
+
+parser = ParserGenerator([l.name for l in LexerGenerator()._lg.rules])
+
+@parser.production('program : statements')
+def program(state: State, p: T) -> T:
+    return p[0]
+
+@parser.production('statements : statement statements')
+def statements(state: State, p: T) -> T:
+    return p[0] + p[1]
